ee823037cb67ec3ed1512df14f0f9df19f1192cc
JasonNguyen47 <158332870+JasonNguyen47@users.noreply.github.com>
2025-04-24 01:42:00 -0500

    Menus work on all Stages now

--- Assets/Scripts/GameManagerButtonHelper.cs
+++ Assets/Scripts/GameManagerButtonHelper.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static PlayerAbility;
 
 public class GameManagerButtonHelper : MonoBehaviour
 {
@@ -33,5 +34,49 @@ public class GameManagerButtonHelper : MonoBehaviour
         GameManager.Instance.RestartGame();
     }
 
+    public void SetAbilityToInvincibility()
+    {
+        GameManager.Instance.SetAbilityToInvincibility();
+    }
+
+    public void SetAbilityToDoubleFireRate()
+    {
+        GameManager.Instance.SetAbilityToDoubleFireRate();
+    }
+
+    public void SetAbilityToSlowAllWallDots()
+    {
+        GameManager.Instance.SetAbilityToSlowAllWallDots();
+    }
+
+    public void SetLeftHandSalt()
+    {
+        GameManager.Instance.SetLeftHandSalt();
+    }
+
+    public void SetLeftHandSpray()
+    {
+        GameManager.Instance.SetLeftHandSpray();
+    }
+
+    public void SetTwoHandSalt()
+    {
+        GameManager.Instance.SetTwoHandSalt();
+    }
+
+    public void SetTwoHandSpray()
+    {
+        GameManager.Instance.SetTwoHandSpray();
+    }
+
+    public void SetRightHandSalt()
+    {
+        GameManager.Instance.SetRightHandSalt();
+    }
+
+    public void SetRightHandSpray()
+    {
+        GameManager.Instance.SetRightHandSpray();
+    }
 
 }
